8880209dd391c3930786f8d581a17553e405f580
Nguyen Ngoc Cong <ngoccong.nncpro@gmail.com>
2022-04-14 10:09:12 +0700

    init layout for guest pages

---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,11 @@
+@extends('guest.layout.master')
+@section('content')
+    <section>
+        <div class="container">
+            <h1>{{ __('Content section') }}</h1>
+        </div>
+    </section>
+    <section></section>
+    <section></section>
+    <section></section>
+@endsection

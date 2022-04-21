310513ce2595e0beb4f01ba20741e4f3a00cc05c
Nguyen Ngoc Cong <ngoccong.nncpro@gmail.com>
2022-04-21 14:09:02 +0700

    init admin layout

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,21 @@
+@extends('admin.layout.master')
+@section('title', __('Dashboard'))
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h1 class="h4">{{ __('Dashboard page') }}</h1>
+                    </div>
+                    <div class="content">
+                        <div class="footer">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection

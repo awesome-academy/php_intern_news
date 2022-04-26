436f436d9ece5c5823d5413fa27f7ed776b41296
Nguyen Ngoc Cong <ngoccong.nncpro@gmail.com>
2022-04-26 09:45:33 +0700

    category-management-ui

--- resources/views/admin/category/sub-category.blade.php
+++ resources/views/admin/category/sub-category.blade.php
@@ -1,6 +1,6 @@
 @foreach ($categories as $category)
     <option value="{{ $category->id }}" @if (old('parent_id') == $category->id) selected @endif>
-        {{ str_repeat('-', $level) . $category->name }}</option>
+        {{ str_repeat('-', $level) . ' ' . $category->name }}</option>
     @includeWhen(count($category->subCategories) > 0, 'admin.category.sub-category', [
         'categories' => $category->subCategories,
         'level' => $level + 1,

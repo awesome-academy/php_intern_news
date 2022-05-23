b5ba9e93cf2d744354234cfcae01a9356c19a130
Nguyen Ngoc Cong <ngoccong.nncpro@gmail.com>
2022-05-23 10:23:59 +0700

    Fix weekly email

--- resources/views/mail/template.blade.php
+++ resources/views/mail/template.blade.php
@@ -11,31 +11,34 @@
 <body>
     <h1 style="color: rgb(54, 91, 212)">{{ __('Weekly Report Email') }}</h1>
     <p style="color: gray">{{ __('Hi') }} <strong style="color:orangered"> {{ $user->name }}</strong>!</p>
-    <p style="color: gray">
-        {{ __("There're :number pending articles, check them out", ['number' => $articles->count()]) }}:</p>
-
-    <table border="1" style="border-collapse: collapse" cellpadding="5px">
-        <thead>
-            <th colspan="2" style="color: gray">{{ __('Title') }}</th>
-            <th style="color: gray">{{ __('Author') }}</th>
-            <th style="color: gray">{{ __('Requested at') }}</th>
-        </thead>
-        <tbody>
-            @foreach ($articles as $index => $article)
-                <tr>
-                    <td style="color: gray">{{ $index + 1 }}</td>
-                    <td style="color: gray">
-                        <a href="{{ route('admin.articles.show', $article->id) }}" target="_blank"
-                            style="text-decoration: none; color:gray; cursor: pointer">
-                            {{ $article->title }}
-                        </a>
-                    </td>
-                    <td style="color: gray">{{ $article->author->name }}</td>
-                    <td style="color: gray">{{ $article->updated_at }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @if ($articles->count())
+        <p style="color: gray">
+            {{ __("There're :number pending articles, check them out", ['number' => $articles->count()]) }}:</p>
+        <table border="1" style="border-collapse: collapse" cellpadding="5px">
+            <thead>
+                <th colspan="2" style="color: gray">{{ __('Title') }}</th>
+                <th style="color: gray">{{ __('Author') }}</th>
+                <th style="color: gray">{{ __('Requested at') }}</th>
+            </thead>
+            <tbody>
+                @foreach ($articles as $index => $article)
+                    <tr>
+                        <td style="color: gray">{{ $index + 1 }}</td>
+                        <td style="color: gray">
+                            <a href="{{ route('admin.articles.show', $article->id) }}" target="_blank"
+                                style="text-decoration: none; color:gray; cursor: pointer">
+                                {{ $article->title }}
+                            </a>
+                        </td>
+                        <td style="color: gray">{{ $article->author->name }}</td>
+                        <td style="color: gray">{{ $article->updated_at }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <h3 style="color: gray">{{ __('There is no pending articles this week!') }}</h3>
+    @endif
     <p style="color: gray">{{ __('From :name', ['name' => config('app.name')]) }}</p>
 </body>
 
